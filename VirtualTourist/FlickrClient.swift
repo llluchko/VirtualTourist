14bbb30646f2314441d9438620a13c94d24a445a
llluchko <l.mladenov21@gmail.com>
2016-08-27 22:30:49 +0300

    Start implementing taskForGetMethod in FlickrClient class

--- VirtualTourist/FlickrClient.swift
+++ VirtualTourist/FlickrClient.swift
@@ -20,4 +20,48 @@ class FlickrClient: NSObject {
 		super.init()
 	}
 	
+	func taskForGetMethodWithParams(parameters: [String: AnyObject], completionHandler: (result: AnyObject!, error: NSError?) -> Void) {
+		
+		// Build the URL and create the request
+		let urlString = Constants.BaseURL + FlickrClient.escapedParameters(parameters)
+		
+		let request = NSMutableURLRequest(URL: NSURL(string: urlString)!)
+		
+		let task = session.dataTaskWithRequest(request) {
+			data, response, downloadError in
+			
+			if let error = downloadError {
+				// TO-DO: Create method errorForResponse
+			} else {
+				// TO-DO: Create method parseJSON
+			}
+		}
+		
+		
+		task.resume()
+		
+	}
+	
+	// Given a dictionary of parameters, convert to a string for a url
+	class func escapedParameters(parameters: [String : AnyObject]) -> String {
+		
+		var urlVars = [String]()
+		
+		for (key, value) in parameters {
+			if(!key.isEmpty) {
+				// Make sure that it is a string value
+				let stringValue = "\(value)"
+				
+				// Escape it
+				let escapedValue = stringValue.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())
+				
+				// Append it
+				urlVars += [key + "=" + "\(escapedValue!)"]
+			}
+			
+		}
+		
+		return (!urlVars.isEmpty ? "?" : "") + urlVars.joinWithSeparator("&")
+	}
+
 }
